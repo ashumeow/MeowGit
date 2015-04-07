1405c28b9c3cd0514b74267eca077eea35883b79
Aswini S <ashumeow@live.com>
2015-04-07 05:55:27 +0530

    init server

---
+++ meow.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"github.com/russross/blackfriday"
+	"path/filepath"
+	"io/ioutil"
+)
+
+func commits() {
+	// add something
+}
+
+func status() {
+	// add something
+}
+
+func details() {
+	// add something
+}
+
+func diff() {
+	// add something
+}
+
+func pull() {
+	// add something
+}
+
+func push() {
+	// add something
+}
+
+func checkout() {
+	// add something
+}
+
+func create_branch() {
+	// add something
+}
+
+func diff_stat() {
+	// add something
+}
+
+func stage() {
+	// add something
+}
+
+func unstage() {
+	// add something
+}
+
+func get_heads_with_commit() {
+	// add something
+}
+
+func get_branches_in_remote() {
+	// add something
+}
+
+func merge() {
+	// add something
+}
+
+func open() {
+	// add something
+}
+
+func run() {
+	// add something
+}
+
+func stop() {
+	// add something
+}
+
+func stopped() {
+	// add something
+}
+
+func index() {
+	// add something
+}
+
+func exit() {
+	// add something
+}
+
+func only_server() {
+	// add something
+}
+
+func terminate_gitj_sig() {
+	// add something
+}
+
+func terminate_gitj() {
+	// add something
+}
+
+func main() {
+    http.ListenAndServe(":8080", http.FileServer(http.Dir(".")))
+}
