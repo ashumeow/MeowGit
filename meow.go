ee35f35918bc1d111a7f8a29976baef42adc8638
Aswini S <ashumeow@live.com>
2015-04-07 15:12:18 +0530

    add some codes

--- meow.go
+++ meow.go
@@ -2,104 +2,164 @@ package main
 
 import (
 	"net/http"
+)
+
+/*
+import (
 	"os"
 	"github.com/russross/blackfriday"
 	"path/filepath"
 	"io/ioutil"
+	"fmt"
 )
+*/
 
-func commits() {
-	// add something
+func commits(w http.ResponseWriter, r *http.Request) {
+	if(r.URL.Path[1:] == "") {
+		// add something
+	}
 }
 
-func status() {
-	// add something
+func status(w http.ResponseWriter, r *http.Request) {
+	if(r.URL.Path[1:] == "") {
+		// add something
+	}
 }
 
-func details() {
-	// add something
+func details(w http.ResponseWriter, r *http.Request) {
+	if(r.URL.Path[1:] == "") {
+		// add something
+	}
 }
 
-func diff() {
-	// add something
+func diff(w http.ResponseWriter, r *http.Request) {
+	if(r.URL.Path[1:] == "") {
+		// add something
+	}
 }
 
-func pull() {
-	// add something
+func pull(w http.ResponseWriter, r *http.Request) {
+	if(r.URL.Path[1:] == "") {
+		// add something
+	}
 }
 
-func push() {
-	// add something
+func push(w http.ResponseWriter, r *http.Request) {
+	if(r.URL.Path[1:] == "") {
+		// add something
+	}
 }
 
-func checkout() {
-	// add something
+func checkout(w http.ResponseWriter, r *http.Request) {
+	if(r.URL.Path[1:] == "") {
+		// add something
+	}
 }
 
-func create_branch() {
-	// add something
+func create_branch(w http.ResponseWriter, r *http.Request) {
+	if(r.URL.Path[1:] == "") {
+		// add something
+	}
 }
 
-func diff_stat() {
-	// add something
+func diff_stat(w http.ResponseWriter, r *http.Request) {
+	if(r.URL.Path[1:] == "") {
+		// add something
+	}
 }
 
-func stage() {
-	// add something
+func stage(w http.ResponseWriter, r *http.Request) {
+	if(r.URL.Path[1:] == "") {
+		// add something
+	}
 }
 
-func unstage() {
-	// add something
+func unstage(w http.ResponseWriter, r *http.Request) {
+	if(r.URL.Path[1:] == "") {
+		// add something
+	}
 }
 
-func get_heads_with_commit() {
-	// add something
+func get_heads_with_commit(w http.ResponseWriter, r *http.Request) {
+	if(r.URL.Path[1:] == "") {
+		// add something
+	}
 }
 
-func get_branches_in_remote() {
-	// add something
+func get_branches_in_remote(w http.ResponseWriter, r *http.Request) {
+	if(r.URL.Path[1:] == "") {
+		// add something
+	}
 }
 
-func merge() {
-	// add something
+func merge(w http.ResponseWriter, r *http.Request) {
+	if(r.URL.Path[1:] == "") {
+		// add something
+	}
 }
 
-func open() {
-	// add something
+func open(w http.ResponseWriter, r *http.Request) {
+	if(r.URL.Path[1:] == "") {
+		// add something
+	}
 }
 
-func run() {
-	// add something
+func run(w http.ResponseWriter, r *http.Request) {
+	if(r.URL.Path[1:] == "") {
+		// add something
+	}
 }
 
-func stop() {
-	// add something
+func stop(w http.ResponseWriter, r *http.Request) {
+	if(r.URL.Path[1:] == "") {
+		// add something
+	}
 }
 
-func stopped() {
-	// add something
+func stopped(w http.ResponseWriter, r *http.Request) {
+	if(r.URL.Path[1:] == "") {
+		// add something
+	}
 }
 
-func index() {
-	// add something
+func index(w http.ResponseWriter, r *http.Request) {
+	if(r.URL.Path[1:] == "") {
+		// add something
+	}
 }
 
-func exit() {
-	// add something
+func exit(w http.ResponseWriter, r *http.Request) {
+	if(r.URL.Path[1:] == "") {
+		// add something
+	}
 }
 
-func only_server() {
-	// add something
+func only_server(w http.ResponseWriter, r *http.Request) {
+	if(r.URL.Path[1:] == "") {
+		// add something
+	}
 }
 
-func terminate_gitj_sig() {
-	// add something
+func terminate_gitj_sig(w http.ResponseWriter, r *http.Request) {
+	if(r.URL.Path[1:] == "") {
+		// add something
+	}
 }
 
-func terminate_gitj() {
-	// add something
+func terminate_gitj(w http.ResponseWriter, r *http.Request) {
+	if(r.URL.Path[1:] == "") {
+		// add something
+	}
 }
 
 func main() {
+
+	// Adding function handlers
+	// For Testing the Go Build
+	http.HandleFunc("/static/templates/_commits.html", commits);
+
+	// TODO: Add more...
+
+	// Event Listener
     http.ListenAndServe(":8080", http.FileServer(http.Dir(".")))
 }
